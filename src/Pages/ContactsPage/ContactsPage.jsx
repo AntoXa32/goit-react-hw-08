4ef0d00e42c8043c01b37a9de264f5794e583405
AntoXa <antoxxa.x@gmail.com>
2024-08-06 20:15:53 +0300

    add task-08

--- src/Pages/ContactsPage/ContactsPage.jsx
+++ src/Pages/ContactsPage/ContactsPage.jsx
@@ -4,8 +4,8 @@ import PageTitle from "../../components/PageTitle/PageTitle";
 import ContactList from "../../components/ContactList/ContactList";
 import { fetchContacts } from "../../redux/contacts/operations";
 import { selectLoading } from "../../redux/contacts/selectors";
-import ContactForm from "../../components/ContactForm/ContactForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
+import ContactForm from "../../components/ContactForm/ContactForm";
 
 export default function ContactsPage() {
   const dispatch = useDispatch();
